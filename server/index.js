4fe5d731db3bf53975210bb4955ce7bc954d2bc5
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-05-29 15:32:30 +0200

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -7,6 +7,21 @@ const port = process.env.Port || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://job-task-user:EWrXLrCmgZaM3W9Z@cluster0.7grdkwx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+
+run();
+
 app.get('/', (req,res) =>{
     res.send('Port is running')
 });
